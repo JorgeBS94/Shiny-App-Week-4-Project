a827ebd06f42509c297ab13b499543a14bc992f1
JorgeBS94 <jorge.bretones.santamarina@gmail.com>
2019-09-04 12:43:37 +0200

    Final versions with percentage error included

--- server.R
+++ server.R
@@ -9,49 +9,61 @@ shinyServer(function(input, output) {
     model1 <- lm(mpg~disp,data = dataset)
     model2 <- lm(mpg~disp+I(disp^2),data = dataset)
     model3 <- lm(mpg~poly(disp,6),data = dataset)
+    dataset <- cbind(dataset,pred1 = predict(model1),pred2 = predict(model2),pred3 = predict(model3))
     
     #Create the plot as an output
     output$my_plot <- renderPlot({
-      g <- ggplot(aes(x = disp,y = mpg),data = dataset) + geom_point() + theme_bw() +
-            labs(x = "Car weight(Kg)",y = "Miles per gallon", title = "Car consumption vs weight") +
+        g <- ggplot(aes(x = disp,y = mpg),data = dataset) + geom_point() + theme_bw() +
+            labs(x = "Car weight(Kg)",y = "Miles per gallon", title = "Fuel consumption vs weight") +
             theme(plot.title = element_text(hjust = 0.5,face = "bold",color = "orange"),axis.title.x = element_text(face = "bold"),
-                axis.title.y = element_text(face = "bold"),legend.text = ) + 
-          scale_color_manual(name="Model",labels=c("Linear","Quadratic","Polynomial 10"),
-                             values=c("red","green","blue"))
-      print(g)
-      #Now we add the regression lines depending on the user inputs
-      if(input$showMod1){
-          dataset <- cbind(dataset,pred1 = predict(model1))
-          g <- g + geom_line(aes(x = disp,y=pred1,colour = "red"),dataset)
-          print(g)
+                axis.title.y = element_text(face = "bold")) 
+        print(g)
+      
+      #We add lines with the correct colours
+        
+      if (input$showMod1){
+          g <- g + geom_line(aes(x = disp,y=pred1,color = "red"),data = dataset)
       }
-      if(input$showMod2){
-          dataset <- cbind(dataset,pred2 = predict(model2))
-          g <- g + geom_line(aes(x = disp,y = pred2,colour = "green"),dataset)
-          print(g)
+      if (input$showMod2){
+          g <- g + geom_line(aes(x = disp,y = pred2,color = "green"),data = dataset)
       }
-      if(input$showMod3){
-          dataset <- cbind(dataset,pred3 = predict(model3))
-          g <- g + geom_line(aes(x = disp,y = pred3,colour = "blue"),dataset)
+      if (input$showMod3){
+          g <- g + geom_line(aes(x = disp,y = pred3,color = "blue"),data = dataset)
+      }
+        
+      #Now we add the correct legend. Ggplot causes a problem, because we need to map a color function
+        #inside aes that does not depend on the data. If we do so, scale_color_manual is not going to 
+        #recognise the colors used as input. 
+        
+      if (input$showMod1 | input$showMod2 | input$showMod3){
+          g <- g + scale_color_identity(name="Model",breaks = c("red","green","blue"),
+                                      labels = c("Linear","Quadratic","Polynomial"),guide = "legend")
           print(g)
       }
     })
     
+    #Add the mean value of the response
+    mean_res <- mean(dataset$mpg)
+    output$Mean_res <- renderText({
+        mean_res
+    })
     #Add the RSE of each model
     output$Mod1 <- renderText({
         if(input$showMod1){
-            summary(model1)$sigma
+            error_1 <- summary(model1)$sigma
+            paste(error_1,"=",round(error_1/mean_res,3)," % training error")
         }
         })
     output$Mod2 <- renderText({
         if(input$showMod2){
-            summary(model2)$sigma
+            error_2<-summary(model2)$sigma
+            paste(error_2,"=",round(error_2/mean_res,3)," % training error")
         }
     })
     output$Mod3 <- renderText({
         if(input$showMod3){
-            summary(model3)$sigma
+            error_3<-summary(model3)$sigma
+            paste(error_3,"=",round(error_3/mean_res,3)," % training error")
         }
     })
-  
 })
--- ui.R
+++ ui.R
@@ -16,13 +16,24 @@ shinyUI(fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("my_plot"),
-       h3("Residual Sum of Squares Linear Regression:"),
-       textOutput("Mod1"),
-       h3("Residual Sum of Squares Quadratic Regression:"),
-       textOutput("Mod2"),
-       h3("Residual Sum of Squares Polynomial 4 Regression:"),
-       textOutput("Mod3")
+        h4("Instructions"),
+        div(style="text-align:justify","We are going to fit 3 different regression models to the mtcars data set. We are interested in the
+        relationship between fuel consumption (in miles/gallon) and the engine displacement (cu.in). Judging from
+        the plot, such a relationship seems non-linear. Hence, we will fit models of increasing complexity and
+        compare their Residual Standard Errors, which should drop when fitting more complicated functions. However, a visual examination
+        of the plots allows to observe that high order polynomials may be overfitting the training data. Hence, they will tend
+        to generalise badly in a test data set that was never observed before. To use the app, simply click on the boxes
+        placed to the left to show one or a combination of the 3 proposed models. This will cause the fitted lines to appear,
+        as well as the estimates of the RSE for the selected models."),
+        plotOutput("my_plot"),
+        h4("Mean value of the response variable (mpg):"),
+        textOutput("Mean_res"),
+        h4("Residual Standard Error (RSE) of Linear Regression:"),
+        textOutput("Mod1"),
+        h4("Residual Standard Error (RSE) of Quadratic Regression:"),
+        textOutput("Mod2"),
+        h4("Residual Standard Error (RSE) of Polynomial Regression:"),
+        textOutput("Mod3")
     )
   )
 ))
